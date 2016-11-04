32aeb1bad0f3bef99571a04f98d96d895146df0e
NGA-IF-SAFETY <ngatruong01021998@gmail.com>
2016-11-04 23:16:39 +0700

    Create bai21_demSoChan.c

---
+++ bai21_demSoChan.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+main()
+
+{
+
+ int i,n;
+
+ printf("moi ban nhap so n \n");
+
+ scanf("%d",&n);
+
+    for (i=1;i<=n;i++)
+
+    if (i%2==0)
+
+ printf("so chan tu 1 den n la: %d\n",i);
+ 
+}
