d839fcfa71af9e2891306169e5b82eb7cbf23f55
NGA-IF-SAFETY <ngatruong01021998@gmail.com>
2016-11-04 17:46:31 +0700

    Create tinhTongn.c

---
+++ tinhTongn.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+
+{
+
+int n;
+
+double s,i;
+
+printf("nhap n vao : ");
+
+scanf("%d",&n);
+
+for (i=1;i<=n;i++)
+    {
+        s+=1/i;
+    }
+
+printf("s=%0.6f ",s);
+
+ }
